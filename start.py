302a23af7637840ff3731c096332e46a69f574d9
Amelorate <ameilorate2@gmail.com>
2016-07-13 19:01:58 -0400

    Replace bare code with main pattern

--- start.py
+++ start.py
@@ -28,29 +28,33 @@ def generate_config_file(name, config):
     print('Config file generated. Edit the username and password fields to set up an account.')
     sys.exit()
 
-# Begin Program
 
-cfg = configparser.ConfigParser()
-if os.path.isfile(CONFIG_FILE):
-    cfg.read(CONFIG_FILE)
-else:
-    generate_config_file(CONFIG_FILE, cfg)
+def main():
+    cfg = configparser.ConfigParser()
+    if os.path.isfile(CONFIG_FILE):
+       cfg.read(CONFIG_FILE)
+    else:
+        generate_config_file(CONFIG_FILE, cfg)
 
-profile = Profile()
-factory = DevotedBotClientFactory()
-if not cfg['auth'].getboolean('auth'):
-    profile.login_offline(cfg['auth']['username'])
-    factory.profile = profile
-else:
-    def login_ok(data):
-        factory.connect(cfg['server']['ip'], int(cfg['server']['port']))
+    profile = Profile()
+    factory = DevotedBotClientFactory()
+    if not cfg['auth'].getboolean('auth'):
+        profile.login_offline(cfg['auth']['username'])
+        factory.profile = profile
+    else:
+        def login_ok(data):
+            factory.connect(cfg['server']['ip'], int(cfg['server']['port']))
 
-    def login_failed(err):
-        print('login failed:', err.value)
-        factory.stop()
-        sys.exit(1)
+        def login_failed(err):
+            print('login failed:', err.value)
+            factory.stop()
+            sys.exit(1)
 
-    factory.profile = profile
-    deferred = profile.login(cfg['auth']['username'], cfg['auth']['password'])
-    deferred.addCallbacks(login_ok, login_failed)
-    factory.run()
+        factory.profile = profile
+        deferred = profile.login(cfg['auth']['username'], cfg['auth']['password'])
+        deferred.addCallbacks(login_ok, login_failed)
+        factory.run()
+
+
+if __name__ == '__main__':
+    main()
