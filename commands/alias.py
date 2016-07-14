e17a01621830fd7c434a5467547815c0ac450828
Amelorate <ameilorate2@gmail.com>
2016-07-14 16:29:50 -0400

    Add alias command

---
+++ commands/alias.py
@@ -0,0 +1,17 @@
+from devbot import chat
+
+
+def call(message, name, protocol, cfg, commands):
+    if message == '':
+        chat.say('/r ' + commands['help']['alias'].format('alias'), protocol)
+        return
+
+    aliases = ''
+    for tupl in commands['regex'].items():
+        if tupl[1] == message:
+            aliases = aliases + tupl[0] + ', '
+    if aliases == '':
+        chat.say('Sorry, there is no command by that name.', protocol)
+    else:
+        chat.say('/r ' + aliases, protocol)
+
