8368703d9701d03e6fc71ffa269a9010c8fd304b
Amelorate <ameilorate2@gmail.com>
2016-07-14 19:53:49 -0400

    Add dice roller

--- commands/dice.py
+++ commands/dice.py
@@ -1,1 +1,14 @@
+import dice
+
+from devbot import chat
+
+
 def call(message, name, protocol, cfg, commands):
+    # noinspection PyBroadException
+    try:
+        result = dice.roll(message)
+        chat.say('/r ' + str(result), protocol)
+    except Exception as e:
+        chat.say('/r Sorry, there was a error in that dice roll:', protocol)
+        if str(e) != '':
+            chat.say('/r ' + str(e), protocol)
