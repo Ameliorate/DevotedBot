fba775709f2b6af646e7fb3eece7576b5257e059
Amelorate <ameilorate2@gmail.com>
2016-07-15 16:41:28 -0400

    Add another quote to the list of quotes

--- commands/quote.py
+++ commands/quote.py
@@ -9,7 +9,8 @@ QUOTES = (
     ("Yeah Gar", "Garking"),
     ("What do you mean? I never hated black people. Ooooh. I see what you mean. I hate niggers with a passion. REEEEEEEEEEEEEEEEEEEEEEEEEE.", "OurTrueLeader"),
     ('this is like saying" OTL is a cool guy, aside from the blatant racism and autism"', "bayernanhaenger"),
-    ("My favourite colours are blue and light blue...", "bigbrainiac10")
+    ("My favourite colours are blue and light blue...", "bigbrainiac10"),
+    ("If I pearl ron_paul I'm naming the post ron_pearl claimed post Garry Oak", "Soerxpso")
 )
 
 
