44e8bda53d258046cc701b88f7be1cc0a6fbdedb
Amelorate <ameilorate2@gmail.com>
2016-07-15 19:03:45 -0400

    Add credits command

---
+++ commands/credit.py
@@ -0,0 +1,7 @@
+from devbot import chat
+
+
+def call(message, name, protocol, cfg, commands):
+    chat.say('/msg {} Amelorate -- Primary Programmer'.format(name))
+    chat.say('/msg {} doughy-goods -- Donated his alt account for DevotedBot'.format(name))
+    chat.say('/msg {} asakuun -- Sizable list of music for music command'.format(name))
