775256b7fce9276f42ac18a19485ea1ad03c0751
Amelorate <ameilorate2@gmail.com>
2016-07-15 21:57:32 -0400

    Make dice not able to freeze the bot with extremely large numbers of dice

--- commands/dice.py
+++ commands/dice.py
@@ -1,17 +1,20 @@
 import dice
 
-from devbot import chat
+from devbot import chat, time_limit, TimeoutException
 from devbot.exception import ChatMessageTooLongError
 
 
 def call(message, name, protocol, cfg, commands):
-    # noinspection PyBroadException
-    try:
-        result = dice.roll(message)
-        chat.say('/msg ' + name + ' ' + str(result))
-    except ChatMessageTooLongError:
-        chat.say('/msg ' + name + ' Sorry, that dicestring gave too many numbers.')
-    except Exception as e:
-        chat.say('/msg ' + name + ' Sorry, there was a error in that dice roll:')
-        if str(e) != '':
-            chat.say_wrap('/msg ' + name + ' ', str(e))
+    with time_limit(3):
+        # noinspection PyBroadException
+        try:
+            result = dice.roll(message)
+            chat.say('/msg ' + name + ' ' + str(result))
+        except ChatMessageTooLongError:
+            chat.say('/msg ' + name + ' Sorry, that dicestring gave too many numbers.')
+        except TimeoutException:
+            chat.say('/msg {} Sorry, that dice roll took too long to execute.'.format(name))
+        except Exception as e:
+            chat.say('/msg ' + name + ' Sorry, there was a error in that dice roll:')
+            if str(e) != '':
+                chat.say_wrap('/msg ' + name + ' ', str(e))
