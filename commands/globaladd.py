70f69f7b801404f7091e91b6ed997602709f9f42
Amelorate <ameilorate2@gmail.com>
2016-07-16 23:37:10 -0400

    Fix gadd not sending tutorial

--- commands/globaladd.py
+++ commands/globaladd.py
@@ -4,11 +4,12 @@ from devbot import chat
 def call(message: str, name, protocol, cfg, commands):
     if message is '':
         chat.say('/msg {} {}'.format(name, commands['help']['globaladd'].format('globaladd')))
-        return 
+        return
     if ' ' in message:
         chat.say('/msg {} Sorry, that was not a valid player name: It contains spaces.'.format(name))
         return
     chat.say('/msg {} Invited {} to GlobalChat'.format(name, message))
-    chat.say_wrap('/msg {}'.format(message),
-                  'You have been added to global chat. Use /g GlobalChat to speak in the group, and /e to exit.')
+    chat.say(
+        '/msg {} You have been invited to global chat. Use /g GlobalChat to enter, and /e to exit.'.format(
+            message))
     chat.say('/nlip GlobalChat {}'.format(message))
