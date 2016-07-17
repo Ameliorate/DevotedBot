dc87c5d000f06e1618c7fbd0daae602131e0602e
Amelorate <ameilorate2@gmail.com>
2016-07-16 23:22:02 -0400

    Fix formatting issues with gadd

--- commands/globaladd.py
+++ commands/globaladd.py
@@ -5,7 +5,7 @@ def call(message: str, name, protocol, cfg, commands):
     if ' ' in message:
         chat.say('/msg {} Sorry, that was not a valid player name: It contains spaces.'.format(name))
         return
-    chat.say_wrap('/msg {}',
-                  'You have been added to global chat. Use /g GlobalChat to speak in the group, and /e to exit.'.format(
-                      message))
+    chat.say('/msg {} Invited {} to GlobalChat'.format(name, message))
+    chat.say_wrap('/msg {}'.format(message),
+                  'You have been added to global chat. Use /g GlobalChat to speak in the group, and /e to exit.')
     chat.say('/nlip GlobalChat {}'.format(message))
