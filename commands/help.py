5526b54ec03c8df4a72f38b5caac793820c8c6a5
Amelorate <ameilorate2@gmail.com>
2016-07-25 17:31:17 -0400

    Make the default behaviour of the help command list commands

--- commands/help.py
+++ commands/help.py
@@ -1,9 +1,26 @@
 from devbot.chat import Chat
 from devbot import DevCommand
+from os import listdir
+from os.path import isfile, join
 
 
 class MainCommand(DevCommand):
     def main(self, args: [str], stdout: Chat, name: str):
+        if len(args) == 1:
+            commandfiles = [f for f in listdir('commands/') if isfile(join('commands/', f))]
+            cmdlist = []
+            for cmd in commandfiles:
+                # Turn filenames into command names and remove __init__.py.
+                if cmd == '__init__.py':
+                    continue
+                split = cmd.split('.')
+                split = split[:-1]  # Remove file extension.
+                name = '.'.join(split)
+                cmdlist.append(name)
+            message = ', '.join(cmdlist)
+            stdout.say('Use `help [command]` to get help on a specific command.')
+            stdout.say(message)
+            return
         try:
             commandmod = __import__('commands.' + args[1], fromlist=['*'])
             stdout.say(commandmod.WIKI)
